1f5de546e46f5dbbc688b442bb834fb07e0d942e
yeste-rge <74136058+yeste-rge@users.noreply.github.com>
2026-07-07 09:41:15 +0000

    add #title in feed.py

--- feed.py
+++ feed.py
@@ -31,6 +31,7 @@ for item in yaml_data['item']:
     xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
     xml_tree.SubElement(item_element, 'pubDate').text = item['published']
     xml_tree.SubElement(item_element, 'title').text = item['title']
+    #xml_tree.SubElement(item_element, 'title').text = item['title']
 
     enclosure = xml_tree.SubElement(item_element, 'enclosure', {
         'url': link_prefix + item['file'],
